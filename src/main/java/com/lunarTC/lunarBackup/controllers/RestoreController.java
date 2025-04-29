a8138f73816c24df5c590f466ccf9e2edc778345
adel <adelselmi8@gmail.com>
2025-04-29 12:01:27 +0100

    preparing the restore part

--- src/main/java/com/lunarTC/lunarBackup/controllers/RestoreController.java
+++ src/main/java/com/lunarTC/lunarBackup/controllers/RestoreController.java
@@ -1,10 +1,67 @@
 package com.lunarTC.lunarBackup.controllers;
 
 
+import com.lunarTC.lunarBackup.configs.GlobalConfigLoader;
+import com.lunarTC.lunarBackup.models.DatabaseConfig;
+import com.lunarTC.lunarBackup.models.GlobalConfig;
+import com.lunarTC.lunarBackup.services.BackupService;
+import com.lunarTC.lunarBackup.services.RestoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/restore")
 public class RestoreController {
+
+    @Autowired
+    GlobalConfigLoader globalConfigLoader;
+
+    @Autowired
+    RestoreService restoreService;
+
+    @GetMapping
+    public String restoreDatabase(@PathVariable String dbName,@PathVariable String dbType,@PathVariable String dataSource){
+
+        GlobalConfig globalConfig=globalConfigLoader.loadGlobalConfig();
+
+        List<DatabaseConfig> databaseConfigs = globalConfig.getDatabaseConfigList();
+
+        for(DatabaseConfig dbConfig :databaseConfigs){
+
+            if(dbConfig.getDatabaseName().equalsIgnoreCase(dbName) && dbConfig.getType().equalsIgnoreCase(dbType))
+            {
+
+                boolean restoreSucceeded=restoreService.restoreDatabase(dbConfig,dataSource);
+                if(restoreSucceeded)
+                {
+                    System.out.println("Successful restore database :"+dbName);
+
+                }
+                else
+                {
+                    System.out.println("failed to restore database :"+dbName);
+
+                }
+
+
+
+
+            }
+
+        }
+
+
+
+
+
+    }
+
+
 
 
 }
--- src/main/java/com/lunarTC/lunarBackup/services/RestoreService.java
+++ src/main/java/com/lunarTC/lunarBackup/services/RestoreService.java
@@ -1,37 +1,92 @@
 package com.lunarTC.lunarBackup.services;
 
-
 import com.lunarTC.lunarBackup.models.DatabaseConfig;
+import com.lunarTC.lunarBackup.utils.DatabaseUtils;
 import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
 
 @Service
 public class RestoreService {
 
+    public Boolean restoreDatabase(DatabaseConfig config, String backupSource) {
+        try {
+            String databaseType = config.getType().toLowerCase();
+            ProcessBuilder processBuilder;
 
-    public Boolean restoreDatabase(DatabaseConfig config,String backupSource)
-    {
+            switch (databaseType) {
+                case "mysql":
+                case "mariadb": {
+                    String mysql = DatabaseUtils.getCachedDumpPath("mysql");
+                    processBuilder = new ProcessBuilder(
+                            mysql,
+                            "-h", config.getHost(),
+                            "-P", String.valueOf(config.getPort()),
+                            "-u", config.getUsername(),
+                            "-p" + config.getPassword(),
+                            config.getDatabaseName()
+                    );
+                    // Redirect input from the SQL file
+                    processBuilder.redirectInput(new File(backupSource));
+                    break;
+                }
 
-        String type=config.getType().toLowerCase();
-        switch(type) {
-            case "mysql":
-            case "mariadb":
+                case "postgres": {
+                    String pgRestore = DatabaseUtils.getCachedDumpPath("pg_restore");
+                    processBuilder = new ProcessBuilder(
+                            pgRestore,
+                            "-h", config.getHost(),
+                            "-p", String.valueOf(config.getPort()),
+                            "-U", config.getUsername(),
+                            "-d", config.getDatabaseName(),
+                            "-c",  // Clean (drop) database objects before recreating
+                            backupSource
+                    );
+                    processBuilder.environment().put("PGPASSWORD", config.getPassword());
+                    break;
+                }
 
+                case "mongo": {
+                    String mongoRestore = DatabaseUtils.getCachedDumpPath("mongorestore");
+                    processBuilder = new ProcessBuilder(
+                            mongoRestore,
+                            "--host", config.getHost(),
+                            "--port", String.valueOf(config.getPort()),
+                            "-u", config.getUsername(),
+                            "-p", config.getPassword(),
+                            "--authenticationDatabase", config.getAuthenticationDatabase(),
+                            "--db", config.getDatabaseName(),
+                            "--drop",   // Drop before restoring
+                            backupSource
+                    );
+                    break;
+                }
 
+                default:
+                    System.out.println("Unsupported database type for restore: " + config.getType());
+                    return false;
+            }
 
-            case "mongo":
+            processBuilder.redirectErrorStream(true);
+            Process process = processBuilder.start();
 
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println("[Restore Log] " + line);
+            }
 
+            int exitCode = process.waitFor();
+            System.out.println("Restore process exited with code: " + exitCode);
 
+            return exitCode == 0;
 
-            case "postgres":
-
-
-
+        } catch (Exception e) {
+            System.err.println("Error during restore: " + e.getMessage());
+            return false;
         }
-
-
-    return false;}
-
-
-
+    }
 }
